f56b000ea36eb95cc6f4aea7a091ada576ca746e
Sumit Rahman <sumitrahman@yahoo.com>
2021-07-08 22:36:15 +0100

    upload original file from local

---
+++ prime_factors.R
@@ -0,0 +1,52 @@
+# coding challenge week ending 9 July
+# Find the prime factors of the input number
+
+list_factors <- function(n) {
+  if (n %in% 2:3) {
+    list_of_proper_factors <- numeric(0)
+  } else {
+    nn <- as.integer(sqrt(n))
+    candidates <- 2:nn
+    are_they_factors <- n %% candidates == 0
+
+    list_of_proper_factors <- candidates[are_they_factors]
+    list_of_proper_factors <-
+      c(list_of_proper_factors, N / list_of_proper_factors)
+    list_of_proper_factors <- unique(list_of_proper_factors)
+  }
+
+  return(list_of_proper_factors)
+}
+
+list_prime_factors <- function(n) {
+  if (!is.numeric(n) |
+    length(n) != 1 | !identical(n, round(n)) | !sign(n) == 1) {
+    stop("Your input must be a single, positive integer")
+  } else if (n == 1) {
+    result <- "1 has no prime factors"
+  } else {
+    first_list <- list_factors(n)
+    second_list <- first_list[vapply(
+      X = first_list, FUN.VALUE = TRUE,
+      FUN = function(x) {
+        length(list_factors(x)) == 0
+      }
+    )]
+
+    if (length(second_list) == 0) {
+      result <- paste0(n, " is a prime number")
+    } else {
+      if (length(second_list) == 1) {
+        result <-
+          paste0(c(n, "has just one prime factor:", second_list),
+            collapse = " "
+          )
+      } else {
+        second_list <- second_list[order(second_list)]
+        result <-
+          paste0(c("The prime factors are:", second_list), collapse = " ")
+      }
+    }
+  }
+  return(result)
+}
